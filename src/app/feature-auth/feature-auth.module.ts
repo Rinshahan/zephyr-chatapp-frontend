c58a60e231e03065f36db4c7f758fbdc56003512
rinshahan <rinshahan18@gmail.com>
2024-02-10 16:38:53 +0530

    adding login and signup functionality

--- src/app/feature-auth/feature-auth.module.ts
+++ src/app/feature-auth/feature-auth.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatureAuthRoutingModule } from './feature-auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
 
 
 @NgModule({
   declarations: [
+    UserLoginComponent,
+    UserSignUpComponent
   ],
   imports: [
     CommonModule,
-    FeatureAuthRoutingModule
+    FeatureAuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class FeatureAuthModule { }
--- src/app/feature-auth/user-sign-up/user-sign-up.component.ts
+++ src/app/feature-auth/user-sign-up/user-sign-up.component.ts
@@ -1,10 +1,29 @@
+import { formatCurrency } from '@angular/common';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { EmailValidator, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-sign-up',
   templateUrl: './user-sign-up.component.html',
   styleUrls: ['./user-sign-up.component.css']
 })
-export class UserSignUpComponent {
+export class UserSignUpComponent implements OnInit {
+  reactiveForm: FormGroup
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      phone: new FormControl(null, Validators.required),
+      image: new FormControl(null),
+      password: new FormControl(null, Validators.required),
+      confirmPassword: new FormControl('', Validators.required)
+    })
+  }
+  onFormSubmitted() {
+    console.log(this.reactiveForm.value);
+    
+  }
+
 
 }
